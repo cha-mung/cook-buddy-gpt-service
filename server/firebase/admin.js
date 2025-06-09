eb88ac4af3b30d2e5789f36c7acc7016aafa8f31
Mori <71653719+cha-mung@users.noreply.github.com>
2025-06-09 22:33:53 +0900

    firebase key 접근 수정

--- server/firebase/admin.js
+++ server/firebase/admin.js
@@ -2,6 +2,9 @@
 import admin from "firebase-admin";
 import fs from "fs";
 import path from "path";
+import dotenv from "dotenv";
+dotenv.config(); 
+
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
@@ -9,8 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 if (!admin.apps.length) {
-  const serviceAccountPath = path.resolve(__dirname, "./serviceAccountKey.json");
-  const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
+  const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
